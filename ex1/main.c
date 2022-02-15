ba93c9317e42d91bae7403edef9a6c5b092c10d2
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-02-15 13:07:05 +0100

    Fixed initial prompt

--- ex1/main.c
+++ ex1/main.c
@@ -2,6 +2,7 @@
 #include <time.h>
 
 #include "alarm_structure.c"
+#include "time_handler.c"
 
 #define SCHEDULE 's'
 #define LIST 'l'
@@ -9,6 +10,18 @@
 #define EXIT 'x'
 
 int main() {
+    time_t current_secs = current_time_as_secs();
+    struct tm *current_time = tm_struct_from_time_t(&current_secs);
+    printf("Welcome to the alarm clock! It is currently %04d-%02d-%02d %02d-%02d-%02d\n",
+    // Add 1900 since tm_year denotes years since 1900
+    current_time -> tm_year + 1900,
+    // Add 1 since tm_mon is 0-indexed
+    current_time -> tm_mon + 1,
+    current_time -> tm_mday,
+    current_time -> tm_hour,
+    current_time -> tm_min,
+    current_time -> tm_sec);
+
 
     // Let the user choose what action to complete
     char choice;
@@ -33,40 +46,9 @@ int main() {
         printf("which is %ld seconds since 1970:\n", new_alarm.num_seconds);
     }
 
-    if (choice == 'l') {
+    if (choice == LIST) {
         
     }
-
-
-    
-    // Declares a time struct to hold the current time as number of seconds
-    //time_t seconds;
-
-    // // When passed the parameter NULL, time defaults to returning the current time
-    // seconds = time(NULL);
-
-    // // Time-and-date struct to hold the current time
-    // struct tm* current_time;
-
-    // // Initialize the time-and-date struct based on current seconds
-    // current_time = localtime(&seconds);
-
-    // // User prompt with a welcome message and the current time
-    // printf("Welcome to the alarm clock! It is currently %04d-%02d-%02d %02d-%02d-%02d\n",
-    // // Add 1900 since tm_year denotes years since 1900
-    // current_time -> tm_year + 1900,
-    // // Add 1 since tm_mon is 0-indexed
-    // current_time -> tm_mon + 1,
-    // current_time -> tm_mday,
-    // current_time -> tm_hour,
-    // current_time -> tm_min,
-    // current_time -> tm_sec);
-
-    // // Prompt user to enter instructin
-    // printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
-    // char instruction;
-    // scanf("%c", &instruction);
-    // printf("You chose to do a %c", instruction);
 
     return 0;
 }
