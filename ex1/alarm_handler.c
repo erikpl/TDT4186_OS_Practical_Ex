a1538731ee3c8af3ec60d735e911ac46fa1dc8d8
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-02-17 21:52:38 +0100

    Changed getPid to set_child_pid

--- ex1/alarm_handler.c
+++ ex1/alarm_handler.c
@@ -4,19 +4,22 @@
 #include <sys/wait.h>
 
 #include "time_handler.c"
+#include "alarm_structure.c"
 
+pid_t child_pid;
+pid_t parent_pid;
 
- pid_t child_pid;
- pid_t parent_pid;
-
-pid_t getPid() {
-        pid_t pid;
-        pid = child_pid;
-        return pid;
-    }
+/**
+ * Sets the PID passed in by reference to the current child PID.
+ * @param[in] pid
+*/
+void set_child_pid(pid_t *pid) {
+    *pid = child_pid;
+}
 
 void set_alarm(Alarm alarm) {
     
+    // Returns the PID of the currently running (parent) process
     parent_pid = getpid();
 
     child_pid = fork();
