fe268afa44faac1a4952c0e9cd7a6d71cbd13450
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-03-16 18:30:46 +0100

    Laget sem_del

--- ex2/sem.c
+++ ex2/sem.c
@@ -59,6 +59,46 @@ SEM *sem_init(int initVal) {
     return semaphore;
 }
 
+/* Destroys a semaphore and frees all associated resources.
+ *
+ * Parameters:
+ *
+ * sem           handle of the semaphore to destroy
+ *
+ * Returns:
+ *
+ * 0 on success, negative value on error. 
+ * -1 = mutex destruction error
+ * -2 = condition destruction error
+ * -3 = both mutex and condition destruction errors
+ *
+ * In case of an error, not all resources may have been freed, but 
+ * nevertheless the semaphore handle must not be used any more.
+ */
+int sem_del(SEM *sem) {
+    int error_counter = 0;
+    /* Attempt to destroy the mutex */
+    if (pthread_mutex_destroy(&(sem -> mutex)) != 0) {
+        error_counter += 1;
+        printf("Couldn't destroy mutex");
+    }
+
+    /* Attempt to destroy the condition */
+    if (pthread_cond_destroy(&(sem -> condition)) != 0) {
+        error_counter += 2;
+        printf("Couldn't destroy condition");
+    }
+
+    /* Regardless of errors, free the semaphore handle */
+    free(sem);
+
+    /* If an error occured, -error_counter will be negative.
+    *  See above for what the different return values mean.
+    *  If not, it will be 0.
+    */
+    return -error_counter;
+}
+
 int main() {
     int init_val1 = 0;
     int init_val2 = 1;
@@ -66,7 +106,12 @@ int main() {
     SEM *semaphore1 = sem_init(init_val1);
     SEM *semaphore2 = sem_init(init_val2);
 
-    printf("\nAddress of semaphore1: %p\n", (void*) semaphore1);
-    printf("\nAddress of semaphore2: %p\n", (void*) semaphore2);
+    printf("Address of semaphore1: %p\n", (void*) semaphore1);
+    printf("Address of semaphore2: %p\n", (void*) semaphore2);    
 
+    int destroy_res1 = sem_del(semaphore1);
+    int destroy_res2 = sem_del(semaphore2);
+
+    printf("\nResult of first destruction: %d", destroy_res1);
+    printf("\nResult of second destruction: %d", destroy_res2);
 }
