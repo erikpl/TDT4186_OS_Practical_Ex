4f5d9f1c331ed6ac0976bef1d222bc3932a48512
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-03-18 17:03:38 +0100

    Implementert P og V. Har ikke testet koden, men den skal være prima.

--- ex2/sem.c
+++ ex2/sem.c
@@ -5,9 +5,12 @@
 
 /* Declare the semaphore struct */
 typedef struct SEM {
+    /* Value of the semaphore.
+     * When the value is 1, execution is allowed.
+     * When the value is 0, it means that another thread is already executing, and the calling thread must wait.
+     * When the value is <0, the number shows how many threads are waiting to execute.
+     */
     volatile int counter;
-    // TODO: not sure if this one is needed
-    volatile int pending_signals;
     pthread_mutex_t mutex;
     pthread_cond_t condition;
 } SEM;
@@ -24,35 +27,35 @@ typedef struct SEM {
  *
  * Returns:
  *
- * handle for the created semaphore, or NULL if an error occured.
+ * handle for the created semaphore, or NULL if an error occurred.
  */
 SEM *sem_init(int initVal) {
     /* Dynamically allocate a semaphore */
     SEM *semaphore = malloc(sizeof(struct SEM));
     /* Check if the dynamic allocation succeeded */
     if (semaphore == NULL) {
-        /* Returns NULL if an error occured */
+        /* Returns NULL if an error occurred */
         return NULL;
     }
     /* Initialize counter to initVal */
     semaphore -> counter = initVal;
     /* Initialize pending_signals to 0 */
-    semaphore -> pending_signals = 0;
+    // semaphore -> pending_signals = 0;
     /* Initialize the mutex to an empty mutex.
-    *  pthread_mutex_init returns an integer not equal to zero of an error occured. 
+    *  pthread_mutex_init returns an integer not equal to zero of an error occurred.
     *  https://pubs.opengroup.org/onlinepubs/7908799/xsh/pthread_mutex_init.html
     */
     if (pthread_mutex_init(&(semaphore -> mutex), NULL) != 0) {
-        /* Returns NULL if an error occured */
+        /* Returns NULL if an error occurred */
         return NULL;
     }
 
     /* Initialize the condition to an empty condition 
-    *  pthread_cond_init returns an integer not equal to zero of an error occured. 
+    *  pthread_cond_init returns an integer not equal to zero of an error occurred.
     *  https://pubs.opengroup.org/onlinepubs/7908799/xsh/pthread_cond_init.html
     */
     if (pthread_cond_init(&(semaphore -> condition), NULL) != 0) {
-        /* Returns NULL if an error occured */
+        /* Returns NULL if an error occurred */
         return NULL;
     }
 
@@ -67,15 +70,13 @@ SEM *sem_init(int initVal) {
  *
  * Returns:
  *
- * 0 on success, negative value on error. 
- * -1 = mutex destruction error
- * -2 = condition destruction error
- * -3 = both mutex and condition destruction errors
+ * 0 on success, negative value on error.
  *
  * In case of an error, not all resources may have been freed, but 
- * nevertheless the semaphore handle must not be used any more.
+ * nevertheless the semaphore handle must not be used anymore.
  */
 int sem_del(SEM *sem) {
+    // TODO: hvordan sjekker man om sem allerede er de-allokert? Må vel legge inn en mekanisme her
     int error_counter = 0;
     /* Attempt to destroy the mutex */
     if (pthread_mutex_destroy(&(sem -> mutex)) != 0) {
@@ -92,11 +93,61 @@ int sem_del(SEM *sem) {
     /* Regardless of errors, free the semaphore handle */
     free(sem);
 
-    /* If an error occured, -error_counter will be negative.
+    /* If an error occurred, -error_counter will be negative.
     *  See above for what the different return values mean.
     *  If not, it will be 0.
     */
     return -error_counter;
+}
+
+/* P (wait) operation.
+ * 
+ * Attempts to decrement the semaphore value by 1. If the semaphore value 
+ * is 0, the operation blocks until a V operation increments the value and 
+ * the P operation succeeds.
+ *
+ * Parameters:
+ *
+ * sem           handle of the semaphore to decrement
+ */
+void P(SEM *sem) {
+    /* Attempts to lock the semaphore's mutex (lock).
+     * If the mutex is already locked, the thread will wait
+     * until the thread is unlocked.
+    */
+    pthread_mutex_lock(&(sem -> mutex));
+
+    while ((sem -> counter) <= 0) {
+        /* Atomically blocks using the semaphore's condition variable.
+         * The calling thread will only resume when another thread signals.
+         * Ignores the return variable.
+        */
+        pthread_cond_wait(&(sem -> condition), &(sem -> mutex));
+    }
+    /* Decrement counter to prevent other threads from executing. */
+    (sem -> counter)--;
+
+    /* Unlock the semaphore's to enable another thread to operate on the semaphore. */
+    pthread_mutex_unlock(&(sem -> mutex));
+}
+
+void V(SEM *sem) {
+    /* Attempts to lock the semaphore's mutex (lock).
+     * If the mutex is already locked, the thread will wait
+     * until the thread is unlocked.
+    */
+    pthread_mutex_lock(&(sem -> mutex));
+
+    /* Increment counter to enable another thread to execute. */
+    (sem -> counter)++;
+
+    /* Unblock a waiting thread calling pthread_cond_wait.
+     * The return variable is ignored.
+    */
+    pthread_cond_signal(&(sem -> condition), &(sem -> mutex));
+
+    /* Unlock the semaphore's to enable another thread to operate on the semaphore. */
+    pthread_mutex_unlock(&(sem -> mutex));
 }
 
 int main() {
@@ -107,11 +158,14 @@ int main() {
     SEM *semaphore2 = sem_init(init_val2);
 
     printf("Address of semaphore1: %p\n", (void*) semaphore1);
-    printf("Address of semaphore2: %p\n", (void*) semaphore2);    
+    printf("Address of semaphore2: %p\n", (void*) semaphore2);
+
 
     int destroy_res1 = sem_del(semaphore1);
     int destroy_res2 = sem_del(semaphore2);
 
     printf("\nResult of first destruction: %d", destroy_res1);
     printf("\nResult of second destruction: %d", destroy_res2);
+
+
 }
